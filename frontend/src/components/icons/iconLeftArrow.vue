d1bf880482c655bfd908997ccf4eeb1af94a7828
Maxwell Muhanda <ajaybullec@gmail.com>
2023-01-16 23:04:10 +0300

    finished about side tab

---
+++ frontend/src/components/icons/iconLeftArrow.vue
@@ -0,0 +1,15 @@
+<template>
+	<svg
+		class="w-3 h-3"
+		width="167"
+		height="274"
+		viewBox="0 0 167 274"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M106.281 136.64L-1.06184e-05 30.3594L30.3597 -0.000283352L167 136.64L30.3597 273.28L-1.32707e-06 242.921L106.281 136.64Z"
+			fill="#607B96"
+		/>
+	</svg>
+</template>
--- frontend/src/components/ui/about/AboutSideTab.vue
+++ frontend/src/components/ui/about/AboutSideTab.vue
@@ -3,7 +3,7 @@
 		<!-- personal info -->
 		<h2 id="accordion-collapse-heading-1">
 			<button
-				@click="openAccordion"
+				@click="togglePersonalInfo"
 				type="button"
 				class="flex items-center pl-6 py-3 md:py-0 mt-3 border-b border-b-lines bg-lines md:bg-inherit space-x-3 w-full font-medium text-left text-white"
 			>
@@ -13,74 +13,115 @@
 				<span :class="classObject">personal-info</span>
 			</button>
 		</h2>
-		<!-- preofesional -->
-		<h2 id="accordion-collapse-heading-1">
-			<button
-				@click="openAccordion"
-				type="button"
-				class="flex items-center pl-6 py-3 md:py-0 mt-3 border-b border-b-lines bg-lines md:bg-inherit space-x-3 w-full font-medium text-left text-white"
+		<!-- personal-body -->
+		<div class="flex flex-col items-start pl-8 mt-5" v-if="personInfoActive">
+			<div
+				class="flex space-x-3 items-center cursor-pointer mb-4 text-gray-200"
 			>
-				<div class="transition duration-500 ease" :class="arrowClick">
-					<iconUpArrow />
-				</div>
-				<span :class="classObject">blog</span>
-			</button>
-		</h2>
-		<!-- preofesional -->
-		<h2 id="accordion-collapse-heading-1">
-			<button
-				@click="openAccordion"
-				type="button"
-				class="flex items-center pl-6 py-3 md:py-0 mt-3 border-b border-b-lines bg-lines md:bg-inherit space-x-3 w-full font-medium text-left text-white"
-			>
-				<div class="transition duration-500 ease" :class="arrowClick">
-					<iconUpArrow />
-				</div>
-				<span :class="classObject">blog</span>
-			</button>
-		</h2>
-
-		<div class="flex flex-col items-start pl-8 mt-5">
-			<div class="flex space-x-3 items-center mb-4 text-gray-200">
-				<div class="transition duration-500 ease" :class="arrowClick">
-					<iconUpArrow />
+				<div class="transition duration-500 ease">
+					<iconLeftArrow />
 				</div>
 				<img src="../../../assets/folder-green.png" alt="" class="w-5" />
-				<p
-					for="default-checkbox"
-					class="ml-2 text-body font-mediumtext-gray-300"
-				>
-					bio
-				</p>
+				<p class="ml-2 text-body font-mediumtext-gray-300">bio</p>
 			</div>
-			<div class="flex space-x-3 items-center mb-4 text-gray-200">
-				<div class="transition duration-500 ease" :class="arrowClick">
-					<iconUpArrow />
+			<div
+				class="flex space-x-3 items-center cursor-pointer mb-4 text-gray-200"
+			>
+				<div class="transition duration-500 ease">
+					<iconLeftArrow />
 				</div>
 				<img src="../../../assets/folder-purple.png" alt="" class="w-5" />
-				<p
-					for="default-checkbox"
-					class="ml-2 text-body font-mediumtext-gray-300"
-				>
-					Tech Stacks
-				</p>
+				<p class="ml-2 text-body font-mediumtext-gray-300">Tech Stacks</p>
 			</div>
-			<div class="flex space-x-3 items-center mb-4 text-gray-200">
-				<div class="transition duration-500 ease" :class="arrowClick">
-					<iconUpArrow />
+			<div
+				@click="toggleMoreInfo"
+				class="flex space-x-3 items-center cursor-pointer mb-4 text-gray-200"
+			>
+				<div class="transition duration-500 ease">
+					<iconLeftArrow />
 				</div>
 				<img src="../../../assets/folder-orange.png" alt="" class="w-5" />
-				<p
-					for="default-checkbox"
-					class="ml-2 text-body font-mediumtext-gray-300"
+				<p class="ml-2 text-body font-mediumtext-gray-300">more about me</p>
+			</div>
+			<div v-if="moreAboutActive">
+				<div
+					class="flex space-x-3 items-center cursor-pointer mb-4 ml-12 text-gray-200"
 				>
-					education
-				</p>
+					<img src="../../../assets/icon-m.png" alt="" class="w-5" />
+					<a
+						href="https://resume.maxino.xyz"
+						class="ml-2 text-body font-mediumtext-gray-300"
+						>resume</a
+					>
+				</div>
+				<div
+					class="flex space-x-3 items-center cursor-pointer mb-4 ml-12 text-gray-200"
+				>
+					<img src="../../../assets/icon-m.png" alt="" class="w-5" />
+					<a
+						href="https://dev.to/maxino22"
+						class="ml-2 text-body font-mediumtext-gray-300"
+						>blog</a
+					>
+				</div>
+			</div>
+		</div>
+
+		<!-- preofesional -->
+		<h2 id="accordion-collapse-heading-1">
+			<button
+				@click="toggleContacts"
+				type="button"
+				class="flex items-center pl-6 py-3 md:py-0 mt-3 border-b border-b-lines bg-lines md:bg-inherit space-x-3 w-full font-medium text-left text-white"
+			>
+				<div class="transition duration-500 ease">
+					<iconLeftArrow />
+				</div>
+				<span :class="classObject">Contacts</span>
+			</button>
+		</h2>
+		<!-- contact-body -->
+		<div class="flex flex-col items-start pl-8 mt-5" v-if="contactsActive">
+			<div
+				class="flex space-x-3 items-center cursor-pointer mb-4 text-gray-200"
+			>
+				<div class="transition duration-500 ease"></div>
+				<iconMail />
+				<p class="ml-2 text-body font-mediumtext-gray-300">bio</p>
+			</div>
+			<div
+				class="flex space-x-3 items-center cursor-pointer mb-4 text-gray-200"
+			>
+				<div class="transition duration-500 ease"></div>
+				<iconPhone />
+				<p class="ml-2 text-body font-mediumtext-gray-300">Tech Stacks</p>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script setup>
+import iconLeftArrow from '../../icons/iconLeftArrow.vue'
 import iconUpArrow from '../../icons/iconUpArrow.vue'
+import iconPhone from '../../icons/iconPhone.vue'
+import iconMail from '../../icons/iconMail.vue'
+import { ref, computed } from 'vue'
+
+const personInfoActive = ref(false)
+const contactsActive = ref(false)
+const moreAboutActive = ref(false)
+
+function togglePersonalInfo() {
+	personInfoActive.value = !personInfoActive.value
+}
+function toggleMoreInfo() {
+	moreAboutActive.value = !moreAboutActive.value
+}
+function toggleContacts() {
+	contactsActive.value = !contactsActive.value
+}
+
+const arrowClick = computed(() => ({
+	'-rotate-180': personInfoActive.value,
+}))
 </script>
