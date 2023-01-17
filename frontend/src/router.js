9280658d468944dfa912c93250401c3a8fd63ee2
Maxwell Muhanda <ajaybullec@gmail.com>
2023-01-17 13:11:15 +0300

    Added sitemap and robots.txt

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-	{ path: '/', component: () => import('./pages/HomePage.vue') },
-	{ path: '/about-me', component: () => import('./pages/AboutPage.vue') },
-	{ path: '/projects', component: () => import('./pages/ProjectsPage.vue') },
-	{ path: '/contact-me', component: () => import('./pages/ContactPage.vue') },
+	{
+		path: '/',
+		meta: {
+			title: 'Maxwell Muhanda | Portfolio Website',
+		},
+		component: () => import('./pages/HomePage.vue'),
+	},
+	{
+		path: '/about-me',
+		component: () => import('./pages/AboutPage.vue'),
+		meta: {
+			title: 'Maxwell Muhanda | About Me',
+		},
+	},
+	{
+		path: '/projects',
+		component: () => import('./pages/ProjectsPage.vue'),
+		meta: {
+			title: 'Maxwell Muhanda | Projects',
+		},
+	},
+	{
+		path: '/contact-me',
+		component: () => import('./pages/ContactPage.vue'),
+		meta: {
+			title: 'Maxwell Muhanda | Contact Me',
+		},
+	},
 ]
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
 	linkActiveClass: 'active',
+	scrollBehavior(_, _2, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		}
+		return {
+			left: 0,
+			top: 0,
+		}
+	},
+})
+
+router.beforeEach((to, from, next) => {
+	document.title = `${to.meta.title}`
+	next()
 })
 
 export default router
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [
+		vue(),
+		// ...
+		Pages({
+			onRoutesGenerated: (routes) => generateSitemap({ routes }),
+			hostname: 'https://maxino.xyz',
+		}),
+	],
 })
