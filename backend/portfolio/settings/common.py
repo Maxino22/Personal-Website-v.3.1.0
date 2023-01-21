7609052040ab3d1775c14d271aee6c7428951e11
Maxwell Muhanda <ajaybullec@gmail.com>
2023-01-21 10:51:28 +0300

    whitenoise error

--- backend/portfolio/settings/common.py
+++ backend/portfolio/settings/common.py
@@ -136,7 +136,8 @@ CLOUDINARY_STORAGE = {
 
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
