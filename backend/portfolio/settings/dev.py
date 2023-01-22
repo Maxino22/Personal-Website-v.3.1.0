08fdb693cad6946464b4edb2b0042600ae708b93
Maxwell Muhanda <ajaybullec@gmail.com>
2023-01-22 12:08:41 +0300

    removed development variables from repositiory

--- backend/portfolio/settings/dev.py
+++
@@ -1,9 +0,0 @@
-from .common import *
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'not_secret'
-
-CORS_ALLOW_ALL_ORIGINS = True
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
