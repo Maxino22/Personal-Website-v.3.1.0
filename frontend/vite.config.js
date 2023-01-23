65619c0feb209883f2314bd7e0325bd64374eaec
Maxwell Muhanda <ajaybullec@gmail.com>
2023-01-23 15:50:46 +0300

    added sitemap and removed unnecessary packages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		// ...
-		Pages({
-			onRoutesGenerated: (routes) => generateSitemap({ routes }),
-			hostname: 'https://maxino.xyz',
-		}),
-	],
+	plugins: [vue()],
 })
