20622833d885b9ddcfd5f818fdfd4bae23b2fbdf
Maxwell Muhanda <ajaybullec@gmail.com>
2023-01-23 15:14:59 +0300

    added google analytics tracking code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,16 @@ import router from './router'
 import BaseCard from './components/ui/BaseCard.vue'
 import pinia from './store'
 import axios from 'axios'
-
+import VueAnalytics from 'vue-analytics'
 const app = createApp(App)
 
 app.component('base-card', BaseCard)
 axios.defaults.baseURL = 'http://localhost:8000/'
 app.use(router)
 app.use(pinia)
+app.use(VueAnalytics, {
+	id: 'G-YEJ6WYZZ47',
+	router,
+})
 
 app.mount('#app')
