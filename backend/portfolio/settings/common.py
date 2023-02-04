f3ebc49f4ea2c04b6735e8649cbdc86f5a05dabe
Maxwell Muhanda <ajaybullec@gmail.com>
2023-02-04 20:34:48 +0300

    celery

--- backend/portfolio/settings/common.py
+++ backend/portfolio/settings/common.py
@@ -137,3 +137,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
