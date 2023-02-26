bbb89b096ee176ffd006d9aaac61295276f4e575
Maxwell Muhanda <67841055+Maxino22@users.noreply.github.com>
2023-02-26 20:32:20 +0300

    removed celery

--- backend/portfolio/celery.py
+++ backend/portfolio/celery.py
@@ -1,9 +1,9 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE' 'portfolio.settlings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE' 'portfolio.settlings')
 
-celery = Celery('portfolio')
-celery.config_from_object('django.conf:settings', namespace='CELERY')
-celery.autodiscover_tasks()
+# celery = Celery('portfolio')
+# celery.config_from_object('django.conf:settings', namespace='CELERY')
+# celery.autodiscover_tasks()
