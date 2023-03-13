d4677b3c375499fcd02aece2b47169b6bb33ed51
Maxwell Muhanda <ajaybullec@gmail.com>
2023-03-13 19:54:10 +0300

    removed cors all

--- backend/portfolio/settings/prod.py
+++ backend/portfolio/settings/prod.py
@@ -7,12 +7,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 ALLOWED_HOSTS = ['api.maxino.xyz', 'localhost']
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:5173',
-#     'https://maxino.xyz',
-#     'https://www.maxino.xyz',
-# ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://maxino.xyz',
+    'https://www.maxino.xyz',
+]
 
 CSRF_TRUSTED_ORIGINS = ['https://api.maxino.xyz']
 
